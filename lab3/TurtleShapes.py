d54a7b6cbf5ad0c29e6d7715a8982b107d7f2627
rani19-meet <rani19@meet.mit.edu>
2017-11-01 19:24:49 +0200

    dfwFWR

---
+++ lab3/TurtleShapes.py
@@ -0,0 +1,27 @@
+import turtle 
+forw=100
+turn=144
+amount=5
+color=['red','blue','yellow','purple','green']
+
+
+for i in range(amount):
+	turtle.pencolor(color[i])
+
+	turtle.forward(forw)
+	turtle.right(turn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.mainloop()
