2cb1232d806af6fcbcf5cb187cefd4cbc3c16096
rani19-meet <rani19@meet.mit.edu>
2017-11-01 18:52:28 +0200

    yay?

---
+++ lab2/lists.py
@@ -0,0 +1,34 @@
+
+def function1(a):
+	b=[]
+	b.append(a[0])
+	b.append(a[-1])
+	print(b)
+function1([1,2,3,4,5,6,7,8,9])
+
+
+a=[1,1,2,3,5,8,13,21,34,55,89]
+for i in range(len(a)):
+	if a[i]<5:
+		print(a[i])
+
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+for value in a:
+	if value in b:
+		print(value)
+
+
+import random
+x=random.randint(7,70)
+y=random.randint(7,70)
+a=[]
+b=[]
+
+
+
+
+
+
