af04ef6b07350f050864b52d1b99fc3cf9e14cc2
rani19-meet <rani19@meet.mit.edu>
2018-01-23 19:27:23 +0200

    uehfweu

--- agario.py
+++ agario.py
@@ -1,112 +1,133 @@
 from turtle import *
 import turtle
+import time
 import random
-from ball import Ball
-hideturtle()
-tracer(0)
-
-#sqrt ( x2-x1 square ) + ( y2-y1 square)
-RUNNING= True
-SLEEP= 0.0077
-SCREEN_WIDTH=turtle.getcanvas().winfo_width()//2
-SCREEN_HEIGHT=turtle.getcanvas().winfo_height()//2
-NUMBER_OF_BALLS = 5
-MINIMUM_BALL_RADIUS = 10
-MAXIMUM_BALL_RADIUS =100
-MINIMUM_BALL_DX = -5
-MAXIMUM_BALL_DX = 5
-MINIMUM_BALL_DY = -5
-MAXIMUM_BALL_DY = 5
-
-BALLS=[]
-
-for i in range(NUMBER_OF_BALLS):
-	x=random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS, SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
-	y=random.randint(-SCREEN_WIDTH + MAXIMUM_BALL_RADIUS, SCREEN_WIDTH - MAXIMUM_BALL_RADIUS)
-	dx=random.randint(MINIMUM_BALL_DX,MAXIMUM_BALL_DX)
-	while dx==0:
-		dx=random.randint(MINIMUM_BALL_DX,MAXIMUM_BALL_DX)
-
-	dy=random.randint(MINIMUM_BALL_DX,MAXIMUM_BALL_DX)
-	while dy==0:
-		dy=random.randint(MINIMUM_BALL_DX,MAXIMUM_BALL_DX)
-	r =random.randint(MINIMUM_BALL_RADIUS, MAXIMUM_BALL_RADIUS)
-	color=(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+from ball import *
+colormode(255)
 
 
 
+number_of_BALLS = 5 
+minimum_ball_radius = 10
+maximum_ball_radius = 100
+minimum_ball_dx = -5
+maximum_ball_dx = 5
+minimum_ball_dy = -5
+maximum_ball_dy = 5
+balls =[]
+
+for i in range(number_of_BALLS):
+
+	screen_random1_x = int(-screen_width+maximum_ball_radius)
+	screen_random2_x = int(screen_width-maximum_ball_radius)
+	random_x = random.randint(screen_random1_x,screen_random2_x)
+	
+	screen_random1_y = int(-screen_height+maximum_ball_radius)
+	screen_random2_y = int(screen_height-maximum_ball_radius)
+	random_y = random.randint(screen_random1_y,screen_random2_y)
+	
+	random_dx = random.randint(minimum_ball_dx,maximum_ball_dx)
+	while random_dx == 0:
+		random_dx = random.randint(minimum_ball_dx,maximum_ball_dx)
+
+	random_dy = random.randint(minimum_ball_dy,maximum_ball_dy)
+	while random_dy == 0:
+		random_dy = random.randint(minimum_ball_dy,maximum_ball_dy)
+	radius = random.randint(minimum_ball_radius,maximum_ball_radius)
+
+	color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+	
 
 
-
-class Ball(Turtle):
-	def __init__(self,x,y,dx,dy,r,color):
-		Turtle.__init__(self)
-		turtle.pu()
-		self.dx = dx
-		self.pu()
-		self.dy = dy
-		self.r = r
-		self.shapesize(r/10)
-		self.goto(x,y)
-		self.color(color)
-		self.shape("circle")
-
-
-	def move(self,width,height):
-		current_x = self.xcor()
-		new_x = current_x + dx 
-		current_y = self.ycor()
-		new_y = current_y + dy
-		right_side_ball = new_x + r
-		left_side_ball = new_x - r
-		top_side_ball = new_y + r
-		bottom_side_ball = new_y - r
-		self.goto(new_x,new_y)
-
-		if top_side_ball > 300:
-			new_y = current_y - dy
-
-		elif bottom_side_ball < -300:
-			new_y = current_y + dy
-
-		elif left_side_ball < -300:
-			new_x = current_x + dx
-
-		elif right_side_ball > 300:
-			new_x = current_x - dx
-
-
+	ball = Ball(random_x,random_y,random_dx,random_dy,radius,color)
+	balls.append(ball)
 
 
 def move_all_balls():
-	for ball in BALLS:
-		ball.move()
-		
-
-#		def top(self):
-#			return self.ycor() + (0.5 *self.height)
-#		def bottom(self):
-#			return self.ycor() - (0.5 * self.height)
-#		def right(self):
-#			return self.xcor() + (0.5 *self.width)
-#		def left(self):
-#			return self.xcor() - (0.5 *self.width)
-
-#if a.top > b.bottom &
-#a.right > b. right &
-#a. bottom < b .bottom &
-#a.left < b.left :
-#return true 
+	for variable in range(number_of_BALLS):
+		balls[variable].move(screen_width,screen_height)
 
 
 
-my_ball = Ball(100,0,5,5,10,"red")
-my_ball.goto(10,10)
+def check_collide(ball_a,ball_b):
+	if ball_a == ball_b:
+		return False
+
+						#squareroot ( x2-x1 squared ) + ( y2-y1 squared)
+	balls_distance = ((ball_a.x-ball_b.x)**2 +(ball_a.y-ball_b.y)**2)**0.5
+
+	if balls_distance+10 <= ball_a.r+ball_b.r:
+		return True
+	else:
+		return False
+
+def check_all_balls_collision():
+	for ball_a in balls:
+		for ball_b in balls:
+			if check_collide(ball_a,ball_b) == True:
+				radius1 = ball_a.r
+				radius2 = ball_b.r
+				random_x = random.randint(screen_random1_x,screen_random2_x)
+				random_y = random.randint(screen_random1_y,screen_random2_y)
+				random_dx = random.randint(minimum_ball_dx,maximum_ball_dx)
+				random_dy = random.randint(minimum_ball_dy,maximum_ball_dy)
+				radius = random.randint(minimum_ball_radius,maximum_ball_radius)
+				color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+				if radius1 > radius2:
+					ball_b.x = random_x
+					ball_b.y = random_y
+					ball_b.dx = random_dx
+					ball_b.dy = random_dy
+					ball_b.r = radius
+					ball_b.color = color
+					radius1 += 1 
+					return False
+				elif radius1 < radius2:
+					ball_a.x = random_x
+					ball_a.y = random_y
+					ball_a.dx = random_dx
+					ball_a.dy = random_dy
+					ball_a.r = radius
+					ball_a.color = color
+					radius2 += 1
+					return False
+
+def check_myball_collision():			
+	for ball in range(balls):
+		random_x = random.randint(screen_random1_x,screen_random2_x)
+		random_y = random.randint(screen_random1_y,screen_random2_y)
+		random_dx = random.randint(minimum_ball_dx,maximum_ball_dx)
+		random_dy = random.randint(minimum_ball_dy,maximum_ball_dy)
+		radius = random.randint(minimum_ball_radius,maximum_ball_radius)
+		color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+		if check_collide(MY_BALL,ball) == True:
+			radius3 = MY_BALL.r
+			radius4 = ball.r
+
+		elif MY_BALL.r < ball.r:
+			return 	False
+		else:
+			MY_BALL.r +=2
+			ball.x = random_x
+			ball.y = random_y
+			ball.dx = random_dx
+			ball.dy = random_dy
+			ball.r = radius
+			ball.color = color
+	return True
+def movearound(event):
+	MY_BALL.x = event.x - screen_width
+	MY_BALL.y = screen_height -event.y
+turtle.listen()
+turtle.getcanvas().bind("<Motion>", movearound)
+for i in range(1000):
+	check_all_balls_collision()
+	move_all_balls()
+	
 
 
+#while running == True:
 
-BALLS.append(my_ball)
-
-
-update()
 mainloop()
